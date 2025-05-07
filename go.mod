6a34998272ac796e92b510331e28b8f30c2cfde4
Arvin <arvintian8@gmail.com>
2025-05-07 11:43:49 +0800

    refactor with go

---
+++ go.mod
@@ -0,0 +1,3 @@
+module goc
+
+go 1.22.11
---
+++ main.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func getRandName(llen int) (string, error) {
+	if llen < 1 {
+		return "", errors.New("invalid length")
+	}
+
+	const letters = "abcdefghijklmnopqrstuvwxyz"
+
+	// 构造随机文件名（含 /tmp/ 前缀）
+	name := "/tmp/"
+	for i := 0; i < llen; i++ {
+		name += string(letters[rand.Intn(len(letters))])
+	}
+
+	return name, nil
+}
+
+func compile(args []string) error {
+	cmd := exec.Command("gcc", args...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("compile error: %v", err)
+	}
+	return nil
+}
+
+func run(filename string, args []string) error {
+	if len(args) == 0 {
+		return nil
+	}
+
+	cmd := exec.Command(filename, args...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("run error: %v", err)
+	}
+	return nil
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: goc file1.c [options] [args]")
+		os.Exit(1)
+	}
+
+	var buildArgs, execArgs []string
+	isCFile := false
+
+	for i := 1; i < len(os.Args); i++ {
+		arg := os.Args[i]
+
+		// 检查是否是.c文件
+		if strings.HasSuffix(arg, ".c") {
+			isCFile = true
+			buildArgs = append(buildArgs, arg)
+			continue
+		}
+
+		// 非.c文件作为运行参数
+		if isCFile {
+			execArgs = append(execArgs, arg)
+		}
+	}
+
+	if !isCFile {
+		fmt.Println("Error: No C files provided")
+		os.Exit(1)
+	}
+
+	// 生成随机文件名
+	exeName, err := getRandName(12)
+	if err != nil {
+		fmt.Printf("Error generating filename: %v\n", err)
+		os.Exit(1)
+	}
+
+	// 构造编译参数
+	compileArgs := append(buildArgs, "-o", exeName, "-lpthread")
+	defer func() {
+		// 删除临时文件
+		if err := os.Remove(exeName); err != nil {
+			fmt.Printf("Error removing file: %v\n", err)
+			os.Exit(1)
+		}
+	}()
+
+	// 编译
+	if err := compile(compileArgs); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// 运行
+	if err := run(exeName, execArgs); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
